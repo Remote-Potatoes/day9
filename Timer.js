97a4ce8e8535350ea5db621530ca069c3008fa91
André de Albuquerque <itstheandre@gmail.com>
2021-07-24 14:29:15 +0200

    DOM + classes

--- Timer.js
+++ Timer.js
@@ -1,15 +1,17 @@
 class Timer {
-  constructor(minutes, seconds) {
+  constructor(minutes, seconds, htmlElement) {
     this.minutes = minutes;
     this.seconds = seconds;
     this.on = false;
     this.intervalId = null;
+    this.htmlElement = htmlElement;
   }
 
   /**
    * Starts the clock, wether it comes from an idle value, or paused value
    */
   start() {
+    console.log("TIMER IS STARTING");
     this.on = true;
 
     this.intervalId = setInterval(() => {
@@ -21,6 +23,7 @@ class Timer {
    * Pauses the timer. Doesn't clear the timer
    */
   pause() {
+    console.log("TIMER GOT PAUSED");
     this.on = false;
     clearInterval(this.intervalId);
   }
@@ -29,32 +32,31 @@ class Timer {
    * Completely clears the timer's clock.
    */
   stop() {
+    console.log("STOP. WAIT A MINUTE. FILL MY CUP PUT SOME LIQUOR IN IT");
     this.on = false;
     this.minutes = 0;
     this.seconds = 0;
     clearInterval(this.intervalId);
+    this.htmlElement.innerText = "0:00";
   }
 
   /**
    * This method is going to be called every second, in case the timer is on
    */
   updateTime() {
-    console.log("UPDATING TIME:");
+    this.htmlElement.innerText = `${this.minutes}:${this.seconds}`;
     if (this.on === false) {
       return;
     }
     if (this.minutes === 0 && this.seconds === 1) {
       this.stop();
-      console.log(`${this.minutes}:${this.seconds}`);
       return;
     }
     if (this.seconds === 0) {
       this.seconds = 59;
       this.minutes -= 1;
-      console.log(`${this.minutes}:${this.seconds}`);
       return;
     }
     this.seconds -= 1;
-    console.log(`${this.minutes}:${this.seconds}`);
   }
 }
--- main.js
+++ main.js
@@ -1,3 +1,97 @@
-const timer = new Timer(10, 0);
-
 // timer.start();
+
+// we want to:
+// by setting a time on minutes we want the timer to have x minutes ready to start counting down
+// upon clicking the button START we want the timer to start counting down
+// upon clicked on pause, we want the timer, to pause, but maintain the timer visible
+// upon clicked the clear button, we want to clear everything
+
+// DOM
+// Document Object Model
+console.log(document);
+console.log(document.body);
+console.log(document.querySelector("h1"));
+const h1 = document.querySelector("h1");
+h1.innerText = "Heyllo";
+
+const newElement = document.createElement("h3");
+newElement.innerText =
+  "Hello, its me. I was wondering if after all these years you'd like to meet";
+
+document.body.appendChild(newElement);
+document.body.removeChild(newElement);
+
+const thatDiv = document.querySelector(".input-container");
+
+// const newInput = document.createElement("input");
+// newInput.type = "date";
+// thatDiv.appendChild(newInput);
+
+// window.addEventListener("click", () => {
+//   console.log("CLICKED");
+// });
+
+// window.onclick = () => {
+//   console.log("CLICKED");
+// };
+
+// window.onmousemove = () => {
+//   console.log("MOUSE IS MOVING");
+// };
+
+const timeContainer = document.querySelector(".time");
+const startBtn = document.querySelector(".start-btn");
+const pauseBtn = document.querySelector(".pause-btn");
+const stopBtn = document.querySelector(".stop-btn");
+const startPause = document.querySelector(".start-pause");
+
+const timer = new Timer(10, 0, timeContainer);
+
+startPause.onclick = () => {
+  startPause.classList.toggle("btn");
+  if (timer.on) {
+    timer.pause();
+    startPause.innerText = "START DYNAMIC";
+    // startPause.classList.add("on");
+    // startPause.classList.remove("off");
+  } else {
+    // startPause.classList.add("off");
+    // startPause.classList.remove("on");
+    timer.start();
+    startPause.innerText = "PAUSE DYNAMIC";
+  }
+};
+
+startBtn.onclick = () => {
+  timer.start();
+};
+
+pauseBtn.onclick = () => {
+  timer.pause();
+};
+
+stopBtn.onclick = () => {
+  timer.stop();
+};
+
+const listOfPeople = [
+  "Anna",
+  "Noemi",
+  "Juan",
+  "Cheryl",
+  "Marie",
+  "Daniel",
+  "Chris",
+  "Filipe",
+  "Pablo2",
+  "Andre",
+];
+
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
+
+listOfPeople.forEach((person) => {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = person;
+  ul.appendChild(newListItem);
+});
